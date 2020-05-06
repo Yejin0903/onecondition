488b7da041e68f6abd61a405055239cc80f3c091
JUDY <s2018s05@e-mirim.hs.kr>
2020-05-06 23:43:17 +0900

    4. 논리 연산자 예제

--- onecondition/Program.cs
+++ onecondition/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,19 @@ namespace onecondition
     {
         static void Main(string[] args)
         {
+            Console.Write("숫자를 입력하세요 : ")
+;            int x = int.Parse(Console.ReadLine());
+            /*if (x > 10) 
+            {
+                if (x < 20)
+                {
+                    Console.WriteLine("조건에 맞습니다.");
+                }
+            }*/
+            if(x>10 && x<20)
+            {
+                Console.WriteLine("조건에 맞습니다.");
+            }
         }
     }
 }
